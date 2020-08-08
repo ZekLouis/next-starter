884371620fb399b49917c4b5b714118bd71cd0dd
Louis Gaume <zeklouis@gmail.com>
2020-08-08 22:30:25 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+const About = () => {
+	return (
+		<section className="hero has-text-light has-background-info">
+			<div className="hero-body">
+				<div className="container">
+					<h1 className="title">
+						About
+					</h1>
+					<h2 className="subtitle">
+						Best starter ever built
+					</h2>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default About;
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+const Index = () => {
+	return (
+		<section className="hero has-text-light has-background-primary">
+			<div className="hero-body">
+				<div className="container">
+					<h1 className="title">
+						Next Starter
+					</h1>
+					<h2 className="subtitle">
+						Best starter ever built
+					</h2>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Index;
